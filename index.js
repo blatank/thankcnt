41d83ee37a3499c74deee5c848292da72d3570e2
blatank <paddocknote@gmail.com>
2019-02-25 13:45:10 +0000

    サーバーから起動できるようにした

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+// 動作確認用HTMLサーバ(結構適当)
+// 要:node.jsのインストール
+// node app.jsを実行してブラウザでlocalhost:8000でアクセス
+
 'use strict';
+const http = require('http');
+const pug = require('pug');
+const server = http.createServer((req, res) => {
+    // debug
+//    console.log(`${req.method} ${req.url}`);
+    
+    // 結構適当
+    // index.htmlに必要なものだったらパイプで渡して読み込む
+    if (   (req.method === 'GET')
+        && (   (req.url === '/index.css')
+            || (req.url === '/thankcnt.js') ) ) {
+        const fs = require('fs');
+        const rs = fs.createReadStream(`.${req.url}`);
+        rs.pipe(res);
+    }
+    // それ以外はindex.htmlを出力
+    else {
+        res.writeHead(200, {
+            'Content-type': 'text/html; charset=utf-8'
+        });
+        res.write(pug.renderFile('./index.pug'));
+        res.end();
+    }
+})
+
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+    console.log('Listening on ' + port);
+});
